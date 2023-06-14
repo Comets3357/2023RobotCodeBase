252f08f12af142f8c0c7150dc68c5367038ddbb2
Hendrik <hnsorens@gmail.com>
2023-06-13 21:32:59 -0400

    Forgot to commit this one thing for the commit below

--- src/main/cpp/lib/Autons.cpp
+++ src/main/cpp/lib/Autons.cpp
@@ -3,100 +3,45 @@
 Autons::Autons(SwerveSubsystem* drivebase)
 {
     swerveSubsystem = drivebase;
-    
-    LoadPaths();
-    LoadAutons();
+
 }
 
 void Autons::RunAuton(std::string autonName)
 {
-
+    if (autons.contains(autonName))
+    autons[autonName].Schedule();
 }
 
-void Autons::LoadPaths()
-{
-    std::filesystem::path directory_path = frc::filesystem::GetDeployDirectory() + "\\pathPlanner\\generatedJSON";
-
-    for (const auto& entry : std::filesystem::directory_iterator(directory_path))
-    {
-        if (entry.is_regular_file())
-        {
-            paths[entry.path().stem().string()] = frc::TrajectoryUtil::FromPathweaverJson(entry.path().string());
-        }
-    }
-}
 
 void Autons::LoadAutons()
 {
-    std::filesystem::path primaryControlPath = frc::filesystem::GetDeployDirectory() + "\\Autons\\PrimaryControl";
-    std::filesystem::path secondaryControlPath = frc::filesystem::GetDeployDirectory() + "\\Autons\\SecondaryControl";
+    std::vector<pathplanner::PathPlannerTrajectory> pathGroup = pathplanner::PathPlanner::loadPathGroup("FullAuto", {pathplanner::PathConstraints{4_mps, 3_mps_sq}});
 
-    for (const auto& entry : std::filesystem::directory_iterator(primaryControlPath))
+    pathplanner::SwerveAutoBuilder autoBuilder(
+        [this]() {return swerveSubsystem->GetPose();},
+        [this](auto initPose) {swerveSubsystem->ResetOdometry(frc::Rotation2d{}, initPose);},
+        pathplanner::PIDConstants(5.0, 0.0, 0.0),
+        pathplanner::PIDConstants(0.5, 0.0, 0.0),
+        [this](auto speeds) {swerveSubsystem->SetChassisSpeed(speeds);},
+        eventMap,
+        {swerveSubsystem},
+        false
+    );
+
+
+    std::string const filePath = frc::filesystem::GetDeployDirectory() + "/pathplanner/";
+    
+    for (const auto& entry : std::filesystem::directory_iterator(filePath))
     {
         if (entry.is_regular_file())
         {
-            std::ifstream file(entry.path().filename().string());
-
-            if (file.is_open())
-            {
-                std::string line;
-
-                while (std::getline(file, line))
-                {
-                    if (line.find("break"))
-                    {
-                        double breakTime = std::stod(line.substr(6));
-                        autons[entry.path().stem().string()].drivebaseCommands.AddCommands(frc2::WaitCommand(units::second_t{breakTime}));
-                    }
-                    else
-                    {
-                        if (paths.contains(line))
-                        {
-                            autons[entry.path().stem().string()].drivebaseCommands.AddCommands(SwerveTrajectoryCommand(paths[line], swerveSubsystem));
-
-                            // pathplanner::PPSwerveControllerCommand path{
-                            //     pathplanner::PathPlannerTrajectory{},
-                            //     [this]() {return swerveSubsystem->GetPose();},
-                            //     frc::PIDController{0.0, 0.0, 0.0},
-                            //     frc::PIDController{0.0, 0.0, 0.0},
-                            //     frc::PIDController{0.0, 0.0, 0.0},
-                            //     [this](frc::ChassisSpeeds speeds) {swerveSubsystem->SetChassisSpeed(speeds);}
-                            // };
-                        }
-                    }
-                }
-            }
+            std::string autonName = entry.path().stem().string();
+            std::vector<pathplanner::PathPlannerTrajectory> pathGroup = pathplanner::PathPlanner::loadPathGroup(autonName, {pathplanner::PathConstraints{4_mps, 3_mps_sq}});
+            autons[autonName] = autoBuilder.fullAuto(pathGroup);
         }
     }
 
-    for (const auto& entry : std::filesystem::directory_iterator(secondaryControlPath))
-    {
-        if (entry.is_regular_file())
-        {
-            std::ifstream file(entry.path().filename().string());
 
-            if (file.is_open())
-            {
-                std::string line;
-
-                while (std::getline(file, line))
-                {
-                    if (line.find("break"))
-                    {
-                        double breakTime = std::stod(line.substr(6));
-                        autons[entry.path().stem().string()].actionCommands.AddCommands(frc2::WaitCommand(units::second_t(breakTime)));
-                    }
-                    else
-                    {
-                        if (commands.contains(line))
-                        {
-                            commands[line](entry.path().stem().string());
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 
